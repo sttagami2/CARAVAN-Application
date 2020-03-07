aa7c7a2ad150751372d2e71942fa4c7143dbdead
sttagami2 <sttagami2@gmail.com>
2020-03-07 16:27:37 +0900

    [Finish] 4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,14 @@
 class BlogsController < ApplicationController
-  def index
-  end
-
+  
   def show
   end
 
+  def index
+    @blogs = Blog.all
+    # @blogsはインスタンス変数であり、取得したデータをViewに渡す役目
+    # Blog.allはblogsテーブルから全てのデータを取得し、インスタンス変数@blogsに代入する役目
+  end
+  
   def new
     @blog = Blog.new
     # 空のインスタンスをViewに渡す
@@ -14,6 +18,9 @@ class BlogsController < ApplicationController
   end
 
   def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
@@ -24,6 +31,6 @@ class BlogsController < ApplicationController
     params.require(:blog).permit(:title, :category, :body)
     # フォームから送られてきた投稿を、createアクションに保存する処理
     # Strong Parametersを使うと、フォームからのデータを受け取れるようになる。
-    # とても大切！　→　覚えておこう
+    # とても大切！ → 覚えておこう
   end
 end
