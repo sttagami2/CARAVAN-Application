e8abf29dd39bfabc0e63276a913ddedee0c29440
vagrant <vagrant@localhost.localdomain>
2020-03-07 05:29:31 +0000

    [Finish] 4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,22 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
+    # 空のインスタンスをViewに渡す
+    # 新規データ登録用のblogインスタンスを、Blogモデルから作成する
+    # Blog.newと記述すると、空のモデルが生成される
+    # インスタンス変数を書いた空のモデルを渡すと、フォームとBlogモデルが関連づけられる
+  end
+
+  def create
   end
 
   def edit
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+    # フォームから送られてきた投稿を、createアクションに保存する処理
   end
 end
