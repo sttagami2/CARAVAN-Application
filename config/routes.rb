b972db3216d3f69eb4e3a1d1d2e0fabe40a2045b
sttagami2 <sttagami2@gmail.com>
2020-03-07 13:54:18 +0900

    [Finish] 4-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs
+#   resources：RESTfulと呼ばれるメソッド
+#   REST：REpresntational State Transfer　→　Webの設計思想の一つ
+#   resourcesはとても便利なメソッド
+#   この一つだけで、7つのメソッドを定義したのと同じ意味を持つ
+#   ・index：リソースの一覧を表示する
+#   ・new：リソースを新規作成する
+#   ・create：リソースを新規作成して追加する
+#   ・edit：リソースを更新するためのフォームを作成する
+#   ・show：レコードの内容を表示する
+#   ・update：リソースを更新する
+#   ・destroy：リソースを削除する
+
 end
